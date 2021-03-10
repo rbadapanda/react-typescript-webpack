33a195399a7ed894b74dd96cc77a4dfffcb78f29
Rajendra Badapanda <badapr@amazon.com>
2021-03-09 17:19:23 -0800

    Added how to toggle top-level-await call

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
+/** un-comment line#3 and comment line #4 to see the top-level-await call not happening */
+// const topLevelAsyncData={data:{first_name:'Raj'}};
 import {topLevelAsyncData} from './TopLevelAwait';
 import * as ReactDOM from "react-dom";
 import { App } from "./App";
-
 console.log('*** Going to render ReactDOM...');
 
 ReactDOM.render(
